97fbed2a648233bb7a44abd23a9ecbe1de576ea3
hakerg <grzegorzabedzki@gmail.com>
2019-04-03 22:07:55 +0200

    Dodaj pliki projektów.

---
+++ Mandelbrot set/Źródło.c
@@ -0,0 +1,109 @@
+#include <allegro5/allegro.h>
+#include <math.h>
+#include <time.h>
+
+bool running = true;
+double reLeft = -2.4;
+double imagTop = -1.5;
+double complexScale = 0.005;
+ALLEGRO_BITMAP *mandelbrot;
+ALLEGRO_EVENT_QUEUE *queue;
+
+int main() {
+	al_init();
+	al_install_keyboard();
+	al_install_mouse();
+
+	al_set_new_window_title("Mandelbrot set");
+	ALLEGRO_DISPLAY *display = al_create_display(960, 600);
+	queue = al_create_event_queue();
+	al_register_event_source(queue, al_get_display_event_source(display));
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_mouse_event_source());
+	mandelbrot = al_create_bitmap(960, 600);
+	int *xArray, *yArray;
+	xArray = malloc(576000 * sizeof(int));
+	yArray = malloc(576000 * sizeof(int));
+	for (int i = 0; i < 576000; i++) {
+		xArray[i] = i % 960;
+		yArray[i] = i / 960;
+	}
+	for (int i = 0; i < 576000; i++) {
+		int index = i + (rand() + rand() * (RAND_MAX + 1)) % (576000 - i);
+		int temp;
+		temp = xArray[i];
+		xArray[i] = xArray[index];
+		xArray[index] = temp;
+		temp = yArray[i];
+		yArray[i] = yArray[index];
+		yArray[index] = temp;
+	}
+
+	int i = 0;
+	double maxIterations = 128.0;
+
+	ALLEGRO_EVENT event;
+	while (running) {
+		while (al_get_next_event(queue, &event)) {
+			switch (event.type) {
+				case ALLEGRO_EVENT_DISPLAY_CLOSE:
+					running = false;
+					break;
+				case ALLEGRO_EVENT_MOUSE_AXES: {
+					if (event.mouse.dz) {
+						double newComplexScale = complexScale * pow(2.0, -event.mouse.dz);
+						reLeft += (complexScale - newComplexScale) * event.mouse.x;
+						imagTop += (complexScale - newComplexScale) * event.mouse.y;
+						complexScale = newComplexScale;
+					}
+					break;
+				}
+			}
+		}
+		al_lock_bitmap(mandelbrot, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_WRITEONLY);
+		al_set_target_bitmap(mandelbrot);
+
+		double re;
+		double imag;
+		double reTemp;
+		double imagTemp;
+		int iterations;
+		double reTempTemp;
+		clock_t timeout = clock() + 0.1 * CLOCKS_PER_SEC;
+		do {
+			int x = xArray[i];
+			int y = yArray[i];
+			re = reLeft + x * complexScale;
+			imag = imagTop + y * complexScale;
+
+			reTemp = 0.0;
+			imagTemp = 0.0;
+			iterations = 0;
+			for (; iterations < maxIterations; iterations++) {
+				reTempTemp = reTemp * reTemp - imagTemp * imagTemp + re;
+				imagTemp = 2 * reTemp * imagTemp + imag;
+				reTemp = reTempTemp;
+				if (sqrt(reTemp * reTemp + imagTemp * imagTemp) >= 2.0) break;
+			}
+			iterations *= 768.0 / maxIterations;
+			if (iterations < 766) {
+				maxIterations *= pow((iterations + 1) / 128.0, 1.0 / 16384.0);
+				if (maxIterations < 128.0) maxIterations = 128.0;
+			}
+			unsigned char r = (iterations >= 255 ? 255 : iterations <= 0 ? 0 : iterations);
+			unsigned char g = (iterations >= 511 ? 255 : iterations <= 256 ? 0 : iterations - 256);
+			unsigned char b = (iterations >= 767 ? 255 : iterations <= 512 ? 0 : iterations - 512);
+			al_put_pixel(x, y, al_map_rgb(r, g, b));
+			
+			i++;
+			if (i == 576000) i = 0;
+		} while (clock() < timeout);
+
+		al_unlock_bitmap(mandelbrot);
+		al_set_target_backbuffer(display);
+		al_draw_bitmap(mandelbrot, 0, 0, 0);
+		al_flip_display();
+	}
+
+	return 0;
+}
