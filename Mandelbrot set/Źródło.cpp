1d470410ca9463da922ee3c7f659ad68f422fac9
hakerg <grzegorzabedzki@gmail.com>
2019-04-04 23:22:20 +0200

    optimisations

--- Mandelbrot set/Źródło.cpp
+++ Mandelbrot set/Źródło.cpp
@@ -4,8 +4,8 @@
 #include <time.h>
 
 bool running = true;
-long double reLeft = -2.4L;
-long double imagTop = -1.5L;
+long double reLeft;
+long double imagTop;
 long double complexScale = 0.005L;
 ALLEGRO_BITMAP *mandelbrot;
 ALLEGRO_EVENT_QUEUE *queue;
@@ -16,19 +16,25 @@ int main() {
 	al_install_mouse();
 
 	al_set_new_window_title("Mandelbrot set");
-	ALLEGRO_DISPLAY *display = al_create_display(960, 600);
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+	ALLEGRO_DISPLAY *display = al_create_display(0, 0);
+	int width = al_get_display_width(display);
+	int height = al_get_display_height(display);
+	int pixelCount = width * height;
+	reLeft = -width * complexScale * 0.5L;
+	imagTop = -height * complexScale * 0.5L;
 	queue = al_create_event_queue();
 	al_register_event_source(queue, al_get_display_event_source(display));
 	al_register_event_source(queue, al_get_keyboard_event_source());
 	al_register_event_source(queue, al_get_mouse_event_source());
-	mandelbrot = al_create_bitmap(960, 600);
-	ALLEGRO_BITMAP *tempMandelbrot = al_create_bitmap(960, 600);
+	mandelbrot = al_create_bitmap(width, height);
+	ALLEGRO_BITMAP *tempMandelbrot = al_create_bitmap(width, height);
 	int *xArray, *yArray;
 	double *pixelisation;
 	ALLEGRO_COLOR *colors;
-	xArray = new int[576000];
-	yArray = new int[576000];
-	pixelisation = new double[576000];
+	xArray = new int[pixelCount];
+	yArray = new int[pixelCount];
+	pixelisation = new double[pixelCount];
 	colors = new ALLEGRO_COLOR[2040];
 	{
 		int r = 0;
@@ -46,13 +52,13 @@ int main() {
 			}
 		}
 	}
-	for (int i = 0; i < 576000; i++) {
-		xArray[i] = i % 960;
-		yArray[i] = i / 960;
+	for (int i = 0; i < pixelCount; i++) {
+		xArray[i] = i % width;
+		yArray[i] = i / width;
 		pixelisation[i] = 0.0;
 	}
-	for (int i = 0; i < 576000; i++) {
-		int index = i + (rand() + rand() * (RAND_MAX + 1)) % (576000 - i);
+	for (int i = 0; i < pixelCount; i++) {
+		int index = i + (rand() + rand() * (RAND_MAX + 1)) % (pixelCount - i);
 		int temp;
 		temp = xArray[i];
 		xArray[i] = xArray[index];
@@ -67,48 +73,55 @@ int main() {
 
 	ALLEGRO_EVENT event;
 	while (running) {
+		int dz = 0;
 		while (al_get_next_event(queue, &event)) {
 			switch (event.type) {
 				case ALLEGRO_EVENT_DISPLAY_CLOSE:
 					running = false;
 					break;
-				case ALLEGRO_EVENT_MOUSE_AXES: {
-					if (event.mouse.dz) {
-						long double relativeScale = powl(2.0L, -event.mouse.dz);
-						long double newComplexScale = complexScale * relativeScale;
-						long double newReLeft = reLeft + (complexScale - newComplexScale) * event.mouse.x;
-						long double newImagTop = imagTop + (complexScale - newComplexScale) * event.mouse.y;
-						al_lock_bitmap(mandelbrot, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READONLY);
-						al_lock_bitmap(tempMandelbrot, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_WRITEONLY);
-						al_set_target_bitmap(tempMandelbrot);
-
-						for (int x = 0; x < 960; x++) {
-							for (int y = 0; y < 600; y++) {
-								int prevX = (x - event.mouse.x) * relativeScale + event.mouse.x;
-								int prevY = (y - event.mouse.y) * relativeScale + event.mouse.y;
-								if (prevX >= 0 && prevX < 960 && prevY >= 0 && prevY < 600) {
-									al_put_pixel(x, y, al_get_pixel(mandelbrot, prevX, prevY));
-									int index = x + y * 960;
-									pixelisation[index] /= relativeScale;
-								}
-								else {
-									pixelisation[x + y * 960] = 0.0;
-								}
-							}
-						}
-
-						al_unlock_bitmap(mandelbrot);
-						al_unlock_bitmap(tempMandelbrot);
-						ALLEGRO_BITMAP *temp = mandelbrot;
-						mandelbrot = tempMandelbrot;
-						tempMandelbrot = temp;
-						reLeft = newReLeft;
-						imagTop = newImagTop;
-						complexScale = newComplexScale;
+				case ALLEGRO_EVENT_KEY_DOWN:
+					if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+						running = false;
 					}
+					break;
+				case ALLEGRO_EVENT_MOUSE_AXES: {
+					dz += event.mouse.dz;
 					break;
 				}
 			}
+		}
+		if (dz) {
+			long double relativeScale = powl(2.0L, -dz);
+			long double newComplexScale = complexScale * relativeScale;
+			long double newReLeft = reLeft + (complexScale - newComplexScale) * event.mouse.x;
+			long double newImagTop = imagTop + (complexScale - newComplexScale) * event.mouse.y;
+			al_lock_bitmap(mandelbrot, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READONLY);
+			al_lock_bitmap(tempMandelbrot, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_WRITEONLY);
+			al_set_target_bitmap(tempMandelbrot);
+
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					long double prevX = (x - event.mouse.x) * relativeScale + event.mouse.x;
+					long double prevY = (y - event.mouse.y) * relativeScale + event.mouse.y;
+					if (prevX >= 0.0L && prevX <= width - 1 && prevY >= 0.0L && prevY <= height - 1) {
+						al_put_pixel(x, y, al_get_pixel(mandelbrot, prevX, prevY));
+						int index = x + y * width;
+						pixelisation[index] /= relativeScale;
+					}
+					else {
+						pixelisation[x + y * width] = 0.0;
+					}
+				}
+			}
+
+			al_unlock_bitmap(mandelbrot);
+			al_unlock_bitmap(tempMandelbrot);
+			ALLEGRO_BITMAP *temp = mandelbrot;
+			mandelbrot = tempMandelbrot;
+			tempMandelbrot = temp;
+			reLeft = newReLeft;
+			imagTop = newImagTop;
+			complexScale = newComplexScale;
 		}
 		al_lock_bitmap(mandelbrot, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_WRITEONLY);
 		al_set_target_bitmap(mandelbrot);
@@ -143,16 +156,16 @@ int main() {
 			else iterations = 1785;
 			ALLEGRO_COLOR color = colors[iterations % 2040];
 			al_put_pixel(x, y, color);
-			pixelisation[x + y * 960] = 1.0;
+			pixelisation[x + y * width] = 1.0;
 			int pix = 2;
 			while (pix < 8) {
 				for (int tx = x / pix * pix; tx < x / pix * pix + pix; tx++) {
 					if (tx < 0) continue;
-					if (tx >= 960) break;
+					if (tx >= width) break;
 					for (int ty = y / pix * pix; ty < y / pix * pix + pix; ty++) {
 						if (ty < 0) continue;
-						if (ty >= 600) break;
-						int index = tx + ty * 960;
+						if (ty >= height) break;
+						int index = tx + ty * width;
 						if (pixelisation[index] > pix || pixelisation[index] == 0.0) {
 							al_put_pixel(tx, ty, color);
 							pixelisation[index] = pix;
@@ -163,7 +176,7 @@ int main() {
 			}
 			
 			i++;
-			if (i == 576000) i = 0;
+			if (i == pixelCount) i = 0;
 		} while (clock() < timeout);
 
 		al_unlock_bitmap(mandelbrot);
